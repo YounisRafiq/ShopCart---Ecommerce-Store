ab3dc109f91a72a37a3c264180bbddbb90388fbb
Younis <ya945055@gmail.com>
2026-05-13 21:38:38 +0500

    feat(App): add logo to ClerkProvider appearance settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@ import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 import { ClerkProvider } from "@clerk/clerk-react";
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
+import logo from "../src/assets/logo.png"
 const App = () => {
   return (
     <div>
-      <ClerkProvider publishableKey={clerkPubKey}>
+      <ClerkProvider publishableKey={clerkPubKey}
+      appearance={{
+        layout : {
+          logoImageUrl : {logo}
+        }
+      }}
+      >
         <AppRoutes />
       </ClerkProvider>
     </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,5 +9,5 @@ import "swiper/css/pagination";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
